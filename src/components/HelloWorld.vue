db72416cfab983a4ff345012e9b579181a094bc4
beepon <36573556+beepon@users.noreply.github.com>
2022-10-08 09:31:22 -0400

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,29 +9,22 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
 
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
+    <div id="app">
+      {{ greeting }}
+    </div>
+    <script type="module" src="/src/main.js"></script>\
+    <script>
+       let App = Vue.createApp({
+           data: function(){
+            return {
+             greeting: 'Hello Vue' 
+            }
+           }  
+       })
+       App.mount('#app');
+    </script>
 
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
-  Healing
 </template>
 
 <style scoped>
